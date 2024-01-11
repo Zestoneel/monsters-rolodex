a7960146949cecd9bcfc688bb2698204013a7260
Zestoneel <shreyashsde@gmail.com>
2024-01-11 21:38:05 +0530

    Adding monster-rolodex starting project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor () {
+    super();
+
+    this.state = {
+      monsters: [
+        
+      ]     
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state)
+      }
+      ));
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            })
+        }}/>
+        {this.state.monsters.map((monster) => {
+            return ( 
+              <h1 key={monster.id}>
+                {monster.name}
+              </h1>
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+export default App;
